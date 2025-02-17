f88593142d57446ec1f261982270b206d9434876
Daniele Falchetti <danielefalche@gmail.com>
2025-02-17 12:24:08 +0100

    Initialize project with Dioxus framework, Tailwind CSS, and basic components

---
+++ src/components/navbar.rs
@@ -0,0 +1,17 @@
+use crate::Route;
+use dioxus::prelude::*;
+
+#[component]
+pub fn Navbar() -> Element {
+    rsx! {
+        div {
+            id: "navbar",
+            Link {
+                to: Route::Home {},
+                "Home"
+            }
+        }
+
+        Outlet::<Route> {}
+    }
+}
