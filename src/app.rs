2e08028c78c187195ffa24cdb9a6d4b7a37d1a4c
Daniele Falchetti <danielefalche@gmail.com>
2025-02-17 14:03:54 +0100

    Update project configuration and integrate Tailwind CSS

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,16 @@
 use crate::route::Route;
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
+const FAVICON: Asset = asset!("/assets/favicon.ico");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 #[component]
 pub(crate) fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Stylesheet {  href: TAILWIND_CSS }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
         Router::<Route> {}
     }
 }
