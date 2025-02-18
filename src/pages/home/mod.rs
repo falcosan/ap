ed17e7db8953805fde8dd1a42f8f657b5b806354
Daniele Falchetti <danielefalche@gmail.com>
2025-02-18 14:17:21 +0100

    Refactor application structure: move main logic to lib.rs and update home page handling

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -15,11 +15,11 @@ pub fn home() -> String {
     let mut env = ENV.lock().unwrap();
 
     env.add_function("increment", |val: i32| {
-        COUNTER.fetch_add(val, Ordering::SeqCst);
+        COUNTER.fetch_add(val, Ordering::SeqCst)
     });
 
     env.add_function("decrement", |val: i32| {
-        COUNTER.fetch_sub(val, Ordering::SeqCst);
+        COUNTER.fetch_sub(val, Ordering::SeqCst)
     });
 
     let template = env.get_template("home.html").unwrap();
