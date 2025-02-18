62908a380b1d9bea9ddb98cd1640c26274c0a8f7
Daniele Falchetti <danielefalche@gmail.com>
2025-02-18 14:20:16 +0100

    Refactor home page template: remove counter display and related logic from home module

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -7,26 +7,20 @@ pub fn home() -> String {
     #[derive(Serialize)]
     struct Props {
         content: String,
-        counter: String,
     }
 
-    static COUNTER: AtomicI32 = AtomicI32::new(0);
-
     let mut env = ENV.lock().unwrap();
+
+    static COUNTER: AtomicI32 = AtomicI32::new(0);
 
     env.add_function("increment", |val: i32| {
         COUNTER.fetch_add(val, Ordering::SeqCst)
     });
 
-    env.add_function("decrement", |val: i32| {
-        COUNTER.fetch_sub(val, Ordering::SeqCst)
-    });
-
     let template = env.get_template("home.html").unwrap();
 
     let page = Props {
-        content: "Lorem Ipsum".into(),
-        counter: COUNTER.load(Ordering::SeqCst).to_string(),
+        content: "Home".into(),
     };
 
     template.render(context!(page)).unwrap()
