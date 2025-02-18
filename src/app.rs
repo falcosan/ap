027762919a92a02ede13f2e83e0e9ea9fc6518aa
Daniele Falchetti <danielefalche@gmail.com>
2025-02-18 01:01:26 +0100

    Implement template rendering in Rust using Minijinja for dynamic content

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,19 @@
+use minijinja::{context, Environment};
 use wasm_bindgen::prelude::*;
 use web_sys::window;
 
 #[wasm_bindgen]
 pub fn app() {
+    let mut env = Environment::new();
+
+    env.add_template("hello.txt", "Hello {{ name }}!").unwrap();
+
+    let template = env.get_template("hello.txt").unwrap();
+    let ea = template.render(context!(name => "DAN")).unwrap();
     if let Some(window) = window() {
         if let Some(document) = window.document() {
             if let Some(element) = document.get_element_by_id("root") {
-                element.set_inner_html("ciao")
+                element.set_inner_html(&ea);
             }
         }
     }
