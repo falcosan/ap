9fbdfa3db71359fc2060801a6416a1e21ce4164e
Daniele Falchetti <danielefalche@gmail.com>
2025-02-19 21:32:12 +0100

    Refactor counter handling to use integer type and simplify CSS styles

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -1,36 +1,37 @@
 use crate::environment::ENV;
+use axum::Json;
 use minijinja::context;
 use serde::Serialize;
 use std::sync::atomic::{AtomicI32, Ordering};
 
 #[derive(Serialize)]
 struct PageContext {
+    counter: i32,
     title: String,
-    counter: String,
 }
 
 static COUNTER: AtomicI32 = AtomicI32::new(0);
 
-fn read_counter() -> String {
-    COUNTER.load(Ordering::SeqCst).to_string()
+fn read_counter() -> i32 {
+    COUNTER.load(Ordering::SeqCst)
 }
 
-pub async fn increase_counter() -> String {
+pub async fn increase_counter() -> Json<i32> {
     let _ = COUNTER.fetch_add(1, Ordering::SeqCst);
-    read_counter()
+    Json(read_counter())
 }
 
-pub async fn decrease_counter() -> String {
+pub async fn decrease_counter() -> Json<i32> {
     let _ = COUNTER.fetch_sub(1, Ordering::SeqCst);
-    read_counter()
+    Json(read_counter())
 }
 
 pub fn home() -> String {
     let env = ENV.lock().unwrap();
     let template = env.get_template("home.html").unwrap();
     let context = PageContext {
-        counter: read_counter(),
         title: "Home".into(),
+        counter: read_counter(),
     };
 
     template.render(context!(page => context)).unwrap()
