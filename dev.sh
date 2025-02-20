25a45d94b08037850a5950f36ed9b5b61cc0b57b
Daniele Falchetti <danielefalche@gmail.com>
2025-02-20 14:46:58 +0100

    Remove unused input.css and simplify dev.sh script for streamlined development

--- dev.sh
+++ dev.sh
@@ -2,15 +2,4 @@
 
 set -euo pipefail
 
-cleanup() {
-    kill $(jobs -p) 2>/dev/null
-    exit 0
-}
-
-trap cleanup SIGINT
-
-cargo watch -q -i .gitignore -i "assets/index.css" -s "cargo run" &
-
-npx @tailwindcss/cli -i ./src/assets/input.css -o ./src/assets/index.css --minify --watch
-
-wait
+cargo watch -q -i .gitignore -s "cargo run"
