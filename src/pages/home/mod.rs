c8ee14ce56613bf18f2b63e7e97dd1f76d704547
Daniele Falchetti <danielefalche@gmail.com>
2025-02-20 14:15:59 +0100

    Refactor home page data handling to support dynamic content and add anyhow for error management

--- src/pages/home/mod.rs
+++ src/pages/home/mod.rs
@@ -1,15 +1,15 @@
 use crate::environment::ENV;
 use minijinja::context;
 use serde::Serialize;
+use serde_json::{from_str, Value};
 use std::env;
 
 #[derive(Serialize)]
-struct PageContext {
-    content: String,
+struct PageContext<T> {
+    content: T,
 }
-
-pub fn get_data_home() -> String {
-    let st_token = env::var("ST_TOKEN").expect("Missing ST_TOKEN");
+pub fn get_data_home() -> Value {
+    let st_token = env::var("ST_TOKEN").unwrap();
 
     let response = minreq::get(format!(
         "https://api.storyblok.com/v2/cdn/stories/home?token={}",
@@ -18,10 +18,14 @@ pub fn get_data_home() -> String {
     .send()
     .expect("Request failed");
 
-    response
-        .as_str()
-        .expect("Failed to convert response to string")
-        .to_string()
+    let json: Value = from_str(
+        response
+            .as_str()
+            .expect("Failed to convert response to string"),
+    )
+    .expect("Failed to convert response to string");
+
+    json["story"].clone()
 }
 
 pub fn home() -> String {
