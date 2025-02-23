c7a830f92e68bd140599ee4cc643bf48072add76
Daniele Falchetti <danielefalche@gmail.com>
2025-02-23 16:05:30 +0100

    Refactor main function to improve dotenv usage and enhance port handling logic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use dotenv::dotenv;
 use std::{env, net::SocketAddr};
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
@@ -15,12 +14,12 @@ mod pages {
 
 #[tokio::main]
 async fn main() {
-    dotenv().ok();
+    dotenv::dotenv().ok();
 
-    let port: u16 = env::var("PORT")
-        .unwrap_or_else(|_| "8000".to_string())
-        .parse()
-        .expect("PORT must be a valid number");
+    let port = env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(8000);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     let listener = TcpListener::bind(addr)
@@ -29,15 +28,13 @@ async fn main() {
 
     println!("Listening on {}", listener.local_addr().unwrap());
 
-    let allowed_origins =
-        std::iter::once(addr.to_string().parse().expect("Invalid default origin"))
-            .chain(["ORIGIN_AP", "ORIGIN_DD"].iter().map(|&var| {
-                env::var(var)
-                    .unwrap_or_else(|_| panic!("Missing environment variable: {}", var))
-                    .parse()
-                    .unwrap_or_else(|_| panic!("Invalid origin provided in {}", var))
-            }))
-            .collect::<Vec<_>>();
+    let allowed_origins = ["ORIGIN_AP", "ORIGIN_DD"]
+        .iter()
+        .filter_map(|&var| env::var(var).ok())
+        .chain(std::iter::once(addr.to_string()))
+        .filter_map(|origin| origin.parse().ok())
+        .collect::<Vec<_>>();
+
     let app = router::router().layer(CorsLayer::new().allow_origin(allowed_origins));
 
     axum::serve(listener, app).await.expect("Server error");
