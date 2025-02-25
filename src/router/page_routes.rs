baee4ad668fb030438a0674e68c041ff7446303a
Daniele Falchetti <danielefalche@gmail.com>
2025-02-25 21:13:54 +0100

    Add XML fetching and response handling for RSS and sitemap routes

--- src/router/page_routes.rs
+++ src/router/page_routes.rs
@@ -2,14 +2,37 @@ use crate::pages::{
     blog::{article, blog},
     home,
 };
-use axum::{extract::Path, response::Html, routing::get, Router};
+use axum::{
+    extract::Path,
+    http::{header::CONTENT_TYPE, HeaderName},
+    response::Html,
+    routing::get,
+    Router,
+};
+use std::{env, error::Error};
 
+fn fetch_xml(url: &str) -> Result<String, Box<dyn Error>> {
+    minreq::get(format!("{}{}", env::var("XML_API")?, url))
+        .send()?
+        .as_str()
+        .map(String::from)
+        .or_else(|_| Err("Failed to read response as string".into()))
+}
+
+fn xml_response(
+    content: Result<String, Box<dyn Error>>,
+) -> ([(HeaderName, &'static str); 1], String) {
+    let content = content.unwrap_or_else(|e| format!("Error: {}", e));
+    ([(CONTENT_TYPE, "application/xml")], content)
+}
 pub fn page_routes(router: Router) -> Router {
     router
-        .route("/", get(Html(home())))
-        .route("/blog", get(Html(blog())))
+        .route("/", get(|| async move { Html(home()) }))
+        .route("/blog", get(|| async move { Html(blog()) }))
         .route(
             "/blog/{slug}",
             get(|params: Path<String>| async move { Html(article(params)) }),
         )
+        .route("/rss.xml", get(xml_response(fetch_xml("rss.xml"))))
+        .route("/sitemap.xml", get(xml_response(fetch_xml("sitemap.xml"))))
 }
